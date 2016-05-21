3de8bb4e52816ffd301ced8869b214f9af44df11
exentio <soularceus@gmail.com>
2016-05-21 23:49:12 +0200

    This could have been worse

---
+++ README.md
@@ -0,0 +1,15 @@
+# DeltaLaunch Strikes Again (aka "My OCD ordered me to make this program")
+My first time doing a C++ application.
+This will be painful. REALLY painful.
+At the moment, it loads a config.ini, if it's a path it launches the program.
+
+## To Do
+* ~Don't cry immediately: **MISHON COMPREE**~
+* Launch apps: **dun**
+* Load config.ini: **dun**
+* Print section of the config.ini except settings: **dun**
+* Launch programs from config.ini: I cant believe that but **dun**
+* Launch websites from config.ini: *to do*
+* GUI: *to do*
+* Set GUI icons: *to do*
+* Set GUI columns: *to do*
---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <Windows.h> // Questa e' la cosa piu' brutta che vedrai nella tua vita.
+#include "SimpleIni.h"
+
+void openApplication(const char* path)
+{
+	ShellExecute(NULL, "open", path, NULL, NULL, SW_SHOW);
+}
+
+int main()
+{
+	int c;
+	char* selection;
+	CSimpleIniA ini;
+	ini.SetUnicode();
+	ini.LoadFile("config.ini");
+	CSimpleIni::TNamesDepend Isections;
+	ini.GetAllSections(Isections);
+	// sections.pop_front();
+	std::vector<CSimpleIni::Entry> sections;
+	Isections.sort(CSimpleIni::Entry::LoadOrder());
+	while (!Isections.empty())
+	{
+		sections.push_back(Isections.front());
+		Isections.pop_front();
+	}
+
+	for (int i = 1; i < sections.size(); ++i)
+	{
+		std::cout << i << ". " << sections.at(i).pItem << '\n';
+	}
+	std::cout << "Inserisci un numero: ";
+	std::cin >> c;
+	selection = (char*) sections.at(c).pItem;
+	CSimpleIni::TNamesDepend IKey;
+	ini.GetAllKeys(selection, IKey);
+	std::vector<CSimpleIni::Entry> key;
+	key.push_back(IKey.front());
+	const char* value = ini.GetValue(selection, key.at(0).pItem, NULL);
+	std::cout << value;
+	if (strcmp(key.at(0).pItem, "path") == 0)
+		openApplication(value);
+	else if (strcmp(key.at(0).pItem, "url") == 0)
+		std::cout << "can't";
+		// openApplication(value); aggiungere parte del browser
+	return 0;
+}
